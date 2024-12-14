e3583ca23a458cc65f6917beeecd8eea181e214c
Vladimir Lynev <djdf112345@yandex.ru>
2024-12-14 18:11:53 +0300

    MouseMovement

--- Assets/Scripts/MouseMove.cs
+++ Assets/Scripts/MouseMove.cs
@@ -1,33 +1,30 @@
 using System.Threading;
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 public class MouseMove : MonoBehaviour
 {
-    float MouseX = 0;
-    float MouseY = 0;
-    float MouseSpeed = 20;
+    public Vector2 CameraLimit = new Vector2(-45, 40);
+
+    float MouseX;
+    float MouseY;
+    float MouseSensivity = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
     private void FixedUpdate()
     {
-        
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        if (gameObject.transform.rotation.x > 40 || gameObject.transform.rotation.x < -40)
-        {
-           
-        }
-        else
-        {
-            MouseX = Input.GetAxis("Mouse X") * MouseSpeed * Time.deltaTime;
-        }
-        
-        MouseY = Input.GetAxis("Mouse Y") * MouseSpeed * Time.deltaTime;
-        transform.rotation *= Quaternion.Euler(MouseY, MouseX, 0);
+        MouseX += Input.GetAxis("Mouse X") * MouseSensivity;
+        MouseY += Input.GetAxis("Mouse Y") * MouseSensivity;
+        // Apply camera limts
+        MouseY = Mathf.Clamp(MouseY, CameraLimit.x, CameraLimit.y);
+
+        transform.rotation = Quaternion.Euler(-MouseY, MouseX, 0);
+
+
+
     }
 }
